eda2d05e1dac5fb52ea0b0ec4a4deead749e00af
Leydi Vidal <afroladymersi2012@gmail.com>
2023-05-20 17:13:06 -0500

    Creacion lista de Contactos con datos predefinidos

---
+++ contact.js
@@ -0,0 +1,18 @@
+// Lista de contactos - 1
+// Crea una lista de contactos con datos predefinidos, cada contacto debe contener el nombre y apellido como una sola cadena de caracteres
+
+let contactList = [
+  "Amari Grace",
+  "Imani Faith",
+  "Kamari Moon",
+  "Zaire River",
+  "Zendaya Sun",
+  "Elijah Jayden ",
+  "Malik Xavier",
+  "Jordan Kyrie",
+  "Caleb Carter",
+  "Laila Amir",
+  "Zuri Serenity",
+];
+
+console.log(contactList);
