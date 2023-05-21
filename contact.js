b4a7cdee31e271ed561f93fb8c5dc13133a74922
Leydi Vidal <afroladymersi2012@gmail.com>
2023-05-20 19:26:15 -0500

    Ajuste de ls funcion  crear un contacto

--- contact.js
+++ contact.js
@@ -18,54 +18,46 @@ let contactList = [
 console.log(contactList);
 
 // Crea una función para añadir un nuevo contacto a una lista
+// Ajusta las funciones de crear y eliminar un contacto para que puedan almacenar la siguiente información como objetos:
+// id, nombres, apellidos, teléfono, ubicaciones, ciudad, dirección
 
-function addContact(contact) {
-  contactList.push(contact);
+function addContact(id, firstName, lastname, phone, location) {
+  let newContact = {
+    id: id,
+    firstName: firstName,
+    lastname: lastname,
+    phone: phone,
+    location: location,
+  };
+
+  contactList.push(newContact);
 }
-addContact("Ashanty Zakur");
+addContact(1, "Sabari", "Zary", 6028768935, "Cali");
 
 console.log(contactList);
-
 
 // Crea una función para borrar un contacto existente de la lista
 
 function deleteContact(contact) {
-    let cont1 = contactList.indexOf(contact);
-    if (cont1 !== -1) {
-      contactList.splice(cont1, 1);
-      console.log(`The contact ${contact} has been eliminated`);
-    } else {
-      console.log(`The contact ${contact} does not exist`);
-    }
+  let cont1 = contactList.indexOf(contact);
+  if (cont1 !== -1) {
+    contactList.splice(cont1, 1);
+    console.log(`The contact ${contact} has been eliminated`);
+  } else {
+    console.log(`The contact ${contact} does not exist`);
   }
-  deleteContact("Caleb Carter");
-  
-  console.log(contactList);
-  
-  // Crea una función para imprimir en consola los contactos presentes en la lista
+}
+deleteContact("Caleb Carter");
+
+console.log(contactList);
+
+// Crea una función para imprimir en consola los contactos presentes en la lista
 
 function printContacts() {
-    console.log("The contacts that this list contains are:");
-    contactList.forEach(function (contact, index) {
-      console.log(index + 1 + ". " + contact);
-    });
-  }
-  
-  printContacts();
-  
-  
+  console.log("The contacts that this list contains are:");
+  contactList.forEach(function (contact, index) {
+    console.log(index + 1 + ". " + contact);
+  });
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+printContacts();
